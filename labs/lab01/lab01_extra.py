547e00fd2ffcef8ca732be24fed13f11cc07149b
Hussain Ali <huali235@gmail.com>
2023-09-12 11:14:18 -0400

    init

---
+++ labs/lab01/lab01_extra.py
@@ -0,0 +1,53 @@
+"""Optional questions for Lab 1"""
+
+# While Loops
+
+
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 0)
+    1
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    """
+    "*** YOUR CODE HERE ***"
+    total = 1
+    while k > 0:
+        total *= n
+        k -= 1
+        n -= 1
+    return total
+
+
+print(falling(4, 0))
+
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    while n > 0:
+        if n % 10 == 8 and n // 10 % 10 == 8:
+            return True
+        n //= 10
+        return False
+
+
+print(double_eights(880088))
